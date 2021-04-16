674316669d85f94c546ea1f3504e92d9f965de89
Majadra <Ran_Kirshenboim@walla.com>
2021-04-16 16:12:27 +0200

    Added march data

--- EsportDashboard/app.R
+++ EsportDashboard/app.R
@@ -2,11 +2,15 @@
 # Preparation
 library(shiny)
 library(shinythemes)
+library(shinydashboard)
 library(RCurl)
 library(tidyverse)
+library(scales)
 library(DT)
+theme_set(theme_bw())
 
-# Reading the datasets
+##############################################################################################
+# Reading the dataset
 General <- 
     read.csv(text = getURL("https://raw.githubusercontent.com/Majadra/Esport.Earning/master/GeneralEsportData.csv"))
 Historical <- 
@@ -24,122 +28,100 @@ General$Game <-
 
 Historical$Game <- 
     as.character(parse(text=shQuote(gsub("<U\\+([A-Z0-9]+)>", "\\\\u\\1",Historical$Game))))
+# Filtering games with low earnings
+gamestorevmove <- General$Game[which(General$TotalEarnings <= 2500)]
 
+General <- General[-which(General$Game %in% gamestorevmove),]
+Historical <- Historical[-which(Historical$Game %in% gamestorevmove),]
 temp <- General %>% select(Game,Genre)
 Historical <- left_join(Historical,temp)
 ##############################################################################################
-ui <- fluidPage(theme = shinytheme("cerulean"),
-    navbarPage("Test",
-               tabPanel("By Game",
-                        
-    column(12,
-           titlePanel(h1("Esport Analysis - Interactive dashboard",align = "center"))
+ui <- dashboardPage(
+    dashboardHeader(title = "Esport Earnings Analysis"),
+    dashboardSidebar(
+        sidebarMenu(
+            uiOutput("Kaggle_link",align = "center"),
+            menuItem("The database",tabName = "db_tab",icon = icon("th"),
+                     menuItem("General data",tabName = "gen_tab"),
+                     menuItem("Historical data",tabName = "hist_tab")),
+            menuItem("Genres",tabName = "genre_tab", icon = icon("dashboard"))
+            
+            
+        ) 
     ),
-
-
-    # Sidebar with a slider input for number of bins 
-    fluidRow(
-            column(4,
-            selectInput(inputId = "Title1",
-                    label = "Select game to analyse :",
-                    choices = c(unique(Historical %>% count(Game, wt = Earnings,sort = TRUE))[[1]][])),offset = 4)),
-          
-
-    fluidRow(column(6,
-            plotOutput("GameTimeline")),
-            column(6,
-            plotOutput("GameTimelineMonth"))), # Row,
-    column(12,
-           DT::dataTableOutput("GenData")), 
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "gen_tab",
+                    DTOutput("db_gen")),
+            tabItem(tabName = "hist_tab",
+                    DTOutput("db_hist")),
+            tabItem(tabName = "genre_tab",
+                    textOutput("Test"),
+                fluidRow(column(width = 4,selectInput(inputId = "in_genre", 
+                                                      label = "Select genre",
+                                                      choices = c("All",Historical %>%
+                                                                      count(Genre, wt = Earnings,sort = TRUE) %>%
+                                                                      select(Genre)))),
+                         column(width = 4,selectInput(inputId = "in_var",
+                                                      label = "Select variable to measure",
+                                                      choices = 
+                                                          c("Total Earnings","Total tournaments","Total players"))),
+                         column(width = 4,selectInput(inputId = "in_time",
+                                                      label = "Select time",
+                                                      choices = c("By year","By month")))), # Fluid row
+                fluidRow(plotOutput("Genre_timeline"))) # genres tab
+        )
+    ) # Body
     
-    ), # Tab n#1
-    tabPanel("By Genre",
-             fluidRow(
-                 column(4,
-                        selectInput(inputId = "Genre1",
-                                    label = "Select genre to analyse :",
-                                    choices = c(unique(Historical %>%
-                                                           count(Genre, wt = Earnings,sort = TRUE))[[1]][])),offset = 4)
-                 ),
-             fluidRow(
-                 column(6,
-                 plotOutput("GenreTimeline")),
-                 column(6,
-                 plotOutput("GenreTournaments"))
-             )
-             # Row
-             ) # Tab n#2
-    
-    
-    
-
- 
-        
-    ) # Nav bar
 ) # UI
 ##############################################################################################
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-output$GameTimeline <- renderPlot({
-    Historical %>%
-        select(Earnings,Date,Game) %>%
-        mutate(Date = as.Date(Date, format = "%d %m %Y"),
-               Year = format(Date,"%Y")) %>%
-        filter(Game == input$Title1) %>%
-        group_by(Year) %>%
-        summarise(Earnings = sum(Earnings)) %>%
-            ggplot(aes(x = Year,y = Earnings)) + 
-            geom_col(fill = "lightblue",color = "black",alpha = 0.75,width = 0.75) +
-            labs(title = "Earnings per year", x = element_blank(), y = element_blank()) +
-            scale_y_continuous(labels = label_comma())
-    })
-
-output$GameTimelineMonth <- renderPlot({
-    Historical %>%
-        select(Earnings,Date,Game) %>%
-        mutate(Date = as.Date(Date, format = "%d %m %Y"),
-               Month = format(Date,"%m")) %>%
-        filter(Game == input$Title1) %>%
-        group_by(Month) %>%
-        summarise(Earnings = sum(Earnings)) %>%
-        ggplot(aes(x = Month,y = Earnings)) + 
-        geom_col(fill = "tomato2",color = "black",alpha = 0.75,width = 0.75) +
-        labs(title = "Earnings per month", x = element_blank(), y = element_blank()) +
-        scale_y_continuous(labels = label_comma())
+    
+Genre_filter <- reactive({
+    if(input$in_genre == "All")
+        {unique(Historical$Genre)}
+    else
+        {input$in_genre}
+})    
+variable <- reactive({
+switch(input$in_var,
+       "Total Earnings" =  sym("Earnings"),
+       "Total tournaments" = sym("Tournaments"),
+       "Total players" = sym("Players"))
 })
 
-output$GenreTimeline <- renderPlot({
+plot1data <- reactive({
     Historical %>%
-        select(Earnings,Date,Genre) %>%
+        select(variable(),Date,Genre) %>%
         mutate(Date = as.Date(Date, format = "%d %m %Y"),
-               Year = format(Date,"%Y")) %>%
-        filter(Genre == input$Genre1) %>%
+               Year = format(Date,ifelse(input$in_time == "By year","%Y","%m"))) %>%
+        filter(Genre %in% Genre_filter()) %>%
         group_by(Year) %>%
-        summarise(Earnings = sum(Earnings)) %>%
-        ggplot(aes(x = Year,y = Earnings)) + 
+        summarise(Total = sum(!!(variable())))
+})
+output$Genre_timeline <- renderPlot({
+    plot1data() %>%
+        ggplot(aes(x = Year,y = Total)) + 
         geom_col(fill = "lightblue",color = "black",alpha = 0.75,width = 0.75) +
-        labs(title = "Earnings", x = element_blank(), y = element_blank()) +
+        labs(title = paste0(input$in_var," - ",input$in_genre), x = element_blank(), y = element_blank()) +
         scale_y_continuous(labels = label_comma())
 })
 
-output$GenreTournaments <- renderPlot({
-    Historical %>%
-        select(Tournaments,Date,Genre) %>%
-        mutate(Date = as.Date(Date, format = "%d %m %Y"),
-               Year = format(Date,"%Y")) %>%
-        filter(Genre == input$Genre1) %>%
-        group_by(Year) %>%
-        summarise(Tournaments = sum(Tournaments)) %>%
-        ggplot(aes(x = Year,y = Tournaments)) + 
-        geom_col(fill = "tomato2",color = "black",alpha = 0.75,width = 0.75) +
-        labs(title = "Tournaments", x = element_blank(), y = element_blank()) +
-        scale_y_continuous(labels = label_comma())
-})
 
-output$GenData <- renderDataTable({
+output$Test <- renderText({paste0(Genre_filter(),input$in_genre)})
+
+output$db_gen <- renderDataTable({
     datatable(General)
-
+    
 })
+output$db_hist <- renderDataTable({
+        datatable(Historical)
+})
+
+url <- tags$a(href="https://www.kaggle.com/rankirsh/esports-earnings-analysis",align = "center","The kaggle notebook")
+output$Kaggle_link <- renderUI({
+    tagList(url) })
 }
 ##############################################################################################
 # Run the application 
